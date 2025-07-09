b63ce1fee395157e547aa536197763489c5b91b4
RovaldaVC <RovaldaVC@gmail.com>
2025-07-09 22:17:37 +0330

    Update main.py
    forgot to add @ and return.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, jsonify, render_template
 
 app = Flask(__name__)
 
-app.route("/")
-def main():
-    render_template("index.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
     
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
